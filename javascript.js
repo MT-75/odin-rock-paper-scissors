522d3fd6e679fab39902f236978bc18a020358c2
MT-75 <199174794+MT-75@users.noreply.github.com>
2025-08-07 21:01:24 -0300

    Add function to get human choice

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
 function getComputerChoice() {
-    let number = Math.floor(Math.random() * 100);
+    let number = Math.floor(Math.random() * 100); // With Math.random() method we can get random choices. //
     if (number <= 33) {
         return "Rock";
     } else if (number <= 66) {
@@ -8,3 +8,10 @@ function getComputerChoice() {
         return "Scissor";
     }
 }
+
+function getHumanChoice() {
+    let humanChoice = prompt("Chose rock, paper or Scissor");
+    humanChoice = humanChoice.toLowerCase(); // With this line the user's input is case insensitive. //
+    humanChoice = humanChoice.slice(0,1).toUpperCase() + humanChoice.slice(1); // Capitalized string. //
+    return humanChoice;
+} 
