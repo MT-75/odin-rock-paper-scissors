36f1cd2b4c4cc013983f6c655287cd0c7be6d5d2
MT-75 <199174794+MT-75@users.noreply.github.com>
2025-08-07 21:43:54 -0300

    Add function to play a round of the game

--- javascript.js
+++ javascript.js
@@ -1,8 +1,8 @@
 function getComputerChoice() {
-    let number = Math.floor(Math.random() * 100); // With Math.random() method we can get random choices. //
-    if (number <= 33) {
+    let computerChoice = Math.floor(Math.random() * 100); // With Math.random() method we can get random choices. //
+    if (computerChoice <= 33) {
         return "Rock";
-    } else if (number <= 66) {
+    } else if (computerChoice <= 66) {
         return "Paper";
     } else {
         return "Scissor";
@@ -18,4 +18,42 @@ function getHumanChoice() {
 
 let humanScore = 0;
 let computerScore = 0;
+
+function playRound (humanChoice, computerChoice) {
+    let humanWinText = `${humanChoice} beats ${computerChoice}. Human wins this round!`;
+    let computerWinText = `${computerChoice} beats ${humanChoice}. Computer win this round!`;
+    if(humanChoice === computerChoice) {
+        console.log("It's a tie!");
+    } else {
+        switch (humanChoice) {
+            case "Rock":
+                if (computerChoice === "Scissor") {
+                    humanScore += 1;
+                    console.log(humanWinText);
+                } else {
+                    computerScore += 1;
+                    console.log(computerWinText);
+                }
+                break
+            case "Paper":
+                if (computerChoice === "Rock") {
+                    humanScore += 1;
+                    console.log(humanWinText);
+                } else {
+                    computerScore += 1;
+                    console.log(computerWinText);
+                }
+                break
+            case "Scissor":
+                 if (computerChoice === "Paper") {
+                    humanScore += 1;
+                    console.log(humanWinText);
+                } else {
+                    computerScore += 1;
+                    console.log(computerWinText);
+                }
+                break
+        }
+    }
+}
 
